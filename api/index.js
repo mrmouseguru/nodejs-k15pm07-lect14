412a3a221274310351ae946b2709cdf5196da0cd
CGM <mrmouse.guru@gmail.com>
2024-03-29 10:20:08 +0700

    enroll functional + MongoDB

--- api/index.js
+++ api/index.js
@@ -93,13 +93,15 @@ myApi.get("/students/:id/courses", async (req, res) => {
   res.json({ courses: courses });
 });
 
-myApi.post("/students/:id/courses", (req, res) => {
+myApi.post("/students/:id/courses", async(req, res) => {
   let student = res.locals.student;
-  let courses = COURSES[student.id];
+ // let courses = COURSES[student.id];
   let code = req.body.code;
   let units = req.body.units;
-  courses.push({ code, units });
+  await Enrollments.insertOne({studentId : student.id, code, units});
+  //courses.push({ code, units });
   student.units += units;
+  await Students.replaceOne({id : student.id}, student);
   res.json({ success: true });
 });
 
